60001c380bf03c0c6f2e03f2409c051024cabce8
Carlos Colina <carlosecolina89@gmail.com>
2024-07-01 20:26:56 -0500

    actualizacion

---
+++ database/migrations/2024_07_02_012623_change_foreign_key_for_user_id_in_user_details2.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0;');
+        try {
+          Schema::table('address_users', function (Blueprint $table) {
+            $table->dropForeign('user_id'); // Elimina la clave foránea
+        });
+        } catch (Exception $e) {
+          //
+        }
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1;');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('address_users', function (Blueprint $table) {
+            $table->foreign('user_id')
+                  ->references('id')
+                  ->on('users')
+                  ->onDelete('cascade'); // Vuelve a crear la clave foránea
+        });
+    }
+};
